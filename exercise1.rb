ac90c0f55346bda092cf3f63ba655a06f8b908a0
Amin Shafiee <Amin@Amins-MacBook-Pro.local>
2016-03-22 14:19:19 -0400

    exercise 1complete

--- exercise1.rb
+++ exercise1.rb
@@ -1,1 +1,15 @@
 puts 2 puts 3 puts 2 + 3
+puts "Hello\t\world"
+puts "Hello\b\b\b\b\bGoodbye world"
+puts "Hello\rStart over world"
+puts "1. Hello\n2. world"
+
+(1 < 3) && (3 < 5)
+
+(1 > 1) && (2 > 2)
+
+(1 == 2) || (2 < 3)
+
+(1 != 2) || (2 < 3)
+
+(1 == 2) || (2 == 3)
