8462049ffeb7e8b16af314a4739b4331322da80c
Amin Shafiee <Amin@Amins-MacBook-Pro.local>
2016-03-22 15:36:30 -0400

    exercise4completed

---
+++ exercise4.rb
@@ -0,0 +1,14 @@
+hundo = (1..100)
+hundo.each do |a|
+  if
+    (a%5 == 0) && (a%3 == 0)
+      puts"Bitmaker"
+  elsif a%5 == 0
+    puts"Makers"
+  elsif
+    a%3 == 0
+  puts "Bit"
+  else
+    puts a
+end
+end
