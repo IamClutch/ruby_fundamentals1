1843e5e958993ffce4d48594799eba48686f31fc
Amin Shafiee <Amin@Amins-MacBook-Pro.local>
2016-03-22 15:39:41 -0400

    exercise3complete

--- exercise3.rb
+++ exercise3.rb
@@ -1,1 +1,29 @@
 puts "What is your name?"
+gets.chomp
+name = "Amin"
+puts "Hi#{name}!"
+
+#Control Structures
+if 2 > 1
+  puts "I get printed!"
+end
+
+if 0 > 1
+  puts "I will not get printed!"
+end
+
+#else
+number = 3
+if number > 0
+  puts "#{number} is positive"
+else
+  puts "#{number} is negative"
+end
+
+number = -4
+
+if number > 0
+  puts "#{number} is positive"
+else
+  puts "#{number} is negative"
+end
